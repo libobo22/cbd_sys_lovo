1426a1bf04f9872ce43bbc71d6c18ef2841cb256
qinlei1006 <245063184@qq.com>
2020-05-11 10:31:10 +0800

    覃磊：实体类目录结构

---
+++ cbd_frontService_sys/src/main/java/com/cbd/commons/bean/CarLeaseBean.java
@@ -0,0 +1,8 @@
+package com.cbd.commons.bean;
+
+
+/**
+ * 车位租凭实体类
+ */
+public class CarLeaseBean {
+}
---
+++ cbd_frontService_sys/src/main/java/com/cbd/commons/bean/PersonalCarBean.java
@@ -0,0 +1,7 @@
+package com.cbd.commons.bean;
+
+/**
+ * 车位实体类
+ */
+public class PersonalCarBean {
+}
