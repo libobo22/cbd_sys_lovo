857ab798cdc38d572f55966bac9b8fb9d8b2c259
HEHANG999 <3139210087@qq.com>
2020-05-13 10:13:45 +0800

    何航：企业合同接口

--- cbd_frontService_sys/src/test/java/companyTest/Test.java
+++ cbd_frontService_sys/src/test/java/companyTest/Test.java
@@ -1,4 +1,33 @@
 package companyTest;
+import com.cbd.companyFront.CompanyMain;
+import com.cbd.companyFront.dto.ContractDto;
+import com.cbd.companyFront.service.ICompanyService;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CompanyMain.class)
 public class Test {
+
+    @Autowired
+    private ICompanyService service;
+
+
+    @org.junit.Test
+    public void findAll(){//查看所有合同
+        List<ContractDto> list = service.findAllContractDto();
+
+        for (ContractDto contractDto : list) {
+            System.out.println(contractDto.toString());
+        }
+
+    }
+
+
+
+
 }
