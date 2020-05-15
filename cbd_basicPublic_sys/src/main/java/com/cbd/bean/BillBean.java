a37a4aafaf2e1d5800297c7a700b22615c22cfb9
86132 <123456>
2020-05-15 15:07:39 +0800

    冯少杰：修改了一点东西

--- cbd_basicPublic_sys/src/main/java/com/cbd/bean/BillBean.java
+++ cbd_basicPublic_sys/src/main/java/com/cbd/bean/BillBean.java
@@ -1,4 +1,15 @@
 package com.cbd.bean;
 
+
+import lombok.Data;
+
+import java.util.Date;
+@Data
 public class BillBean {
+    private int billID;
+    private String billType;
+    private float expend;
+    private float income;
+    private String dealRemark;
+    private Date dealTime;
 }
