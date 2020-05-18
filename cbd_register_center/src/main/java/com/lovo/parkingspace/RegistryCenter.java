243affd89b24cbb34f3b7c485a2514ed1d32e897
86132 <123456>
2020-05-18 14:32:19 +0800

    冯少杰：添加个人用户

--- cbd_register_center/src/main/java/com/lovo/parkingspace/RegistryCenter.java
+++ cbd_register_center/src/main/java/com/lovo/parkingspace/RegistryCenter.java
@@ -4,12 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * Eureka 服务注册中心
- *
- * @author liren
- * @date 2020-04-30
- */
 @SpringBootApplication
 @EnableEurekaServer
 public class RegistryCenter {
