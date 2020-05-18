74186b936e9769617c8284c6c8eb922f275ac8ca
86132 <123456>
2020-05-18 11:16:16 +0800

    冯少杰：web前端服务器

---
+++ cbd_Web/src/main/java/com/cbd/WebMain.java
@@ -0,0 +1,16 @@
+package com.cbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients(basePackages = {"com.cbd.service"})
+public class WebMain {
+    public static void main(String[] args) {
+        SpringApplication.run(WebMain.class);
+    }
+}
